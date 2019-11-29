ba24e0daf11c446b5e85cad8bd5436f5ef63aa81
Jasper van Merle <jaspervmerle@gmail.com>
2019-11-29 03:45:01 +0100

    Add docset selector with search

---
+++ src/docsets.ts
@@ -0,0 +1,45 @@
+import axios from 'axios';
+
+export interface DocsetAuthor {
+  name: string;
+  link: string;
+}
+
+export interface DocsetVersion {
+  version: string;
+  archive: string;
+  author?: DocsetAuthor;
+}
+
+export interface Docset {
+  // Directory-name friendly
+  id: string;
+
+  // Display friendly, possibly directory-name unfriendly
+  name: string;
+
+  version: string;
+  specific_version: DocsetVersion[];
+
+  archive: string;
+  aliases: string[];
+
+  // Base64 strings of the icon
+  icon: string;
+  'icon@2x': string;
+
+  author: DocsetAuthor;
+}
+
+export async function getAvailableDocsets(): Promise<Docset[]> {
+  const response = await axios.get('https://kapeli.com/feeds/zzz/user_contributed/build/index.json', {
+    responseType: 'json',
+  });
+
+  return Object.keys(response.data.docsets).map(key => {
+    return {
+      id: key,
+      ...response.data.docsets[key],
+    };
+  });
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import { flags } from '@oclif/command';
+import * as inquirer from 'inquirer';
 import { BaseCommand } from './BaseCommand';
+import { Docset, getAvailableDocsets } from './docsets';
 import { getDocsetsDirectory } from './zeal';
+
+// tslint:disable-next-line:no-var-requires
+inquirer.registerPrompt('autocomplete', require('inquirer-autocomplete-prompt'));
 
 class ZealUserContrib extends BaseCommand {
   public static description = "conveniently add Dash's User Contributed docsets to Zeal";
@@ -14,15 +19,49 @@ class ZealUserContrib extends BaseCommand {
   };
 
   public async run(): Promise<void> {
-    let docsetsDirectory: string = this.flags['output-directory'];
-
-    if (docsetsDirectory === undefined) {
-      try {
-        docsetsDirectory = getDocsetsDirectory();
-      } catch (err) {
-        this.err(err.message);
-      }
+    try {
+      this.runSafe();
+    } catch (err) {
+      this.err(err.message);
     }
+  }
+
+  private async runSafe(): Promise<void> {
+    let docsetsDirectory: string = this.flags['output-directory'];
+    if (docsetsDirectory === undefined) {
+      docsetsDirectory = getDocsetsDirectory();
+    }
+
+    const availableDocsets = await getAvailableDocsets();
+    const selectedDocset = await this.selectDocset(availableDocsets);
+
+    this.log(JSON.stringify(selectedDocset, null, 2));
+  }
+
+  private async selectDocset(availableDocsets: Docset[]): Promise<Docset> {
+    const docsetNames = availableDocsets
+      .map(docset => docset.name)
+      .sort((a, b) => {
+        return a.toLowerCase().localeCompare(b.toLowerCase());
+      });
+
+    const { selectedName } = await inquirer.prompt([
+      {
+        type: 'autocomplete',
+        name: 'selectedName',
+        message: 'Select a docset to add to Zeal',
+        source: async (answersSoFar: string[], input: string) => {
+          if (input === undefined) {
+            return docsetNames;
+          }
+
+          input = input.toLowerCase();
+          return docsetNames.filter(name => name.toLowerCase().includes(input));
+        },
+      },
+    ]);
+
+    return availableDocsets.find(docset => docset.name === selectedName);
   }
 }
 
